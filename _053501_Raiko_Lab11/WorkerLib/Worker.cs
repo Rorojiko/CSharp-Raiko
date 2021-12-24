1245a7f84a8430e682d5c0246bdbcf57d8792905
Rorojiko <62502281+Rorojiko@users.noreply.github.com>
2021-12-24 23:29:56 +0300

    lab 9-11

---
+++ _053501_Raiko_Lab11/WorkerLib/Worker.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkerLib
+{
+    public class Worker
+    {
+        public int Id;
+        public string Name;
+        public int Age;
+
+        public Worker(int id, int age, string name)
+        {
+            this.Age = age;
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public string Info()
+        {
+            string res = $"Id: {Id}\nName: {Name}\nAge: {Age}\n";
+            res += "--------------------------";
+            return res;
+        }
+    }
+}
